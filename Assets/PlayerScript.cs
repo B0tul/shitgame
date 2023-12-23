6c67243210170d02ed0ae0f9f5dfa21dbd52db74
Marat <nmg1033@gmail.com>
2023-12-24 00:16:00 +0300

    Fixed

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -22,20 +22,22 @@ public class PlayerScript : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.UpArrow) == true) // up
         {
-            myRigidbody.velocity = transform.TransformVector(new Vector3(0, 1 * speed, 0)) * Time.deltaTime;
+            myRigidbody.velocity = transform.TransformVector(new Vector3(0, 1, 0)) * speed;
         }
         if (Input.GetKeyUp(KeyCode.UpArrow) == true) // up (disable)
         {
-            myRigidbody.velocity = transform.TransformVector(new Vector3(0, 0, 0));
+            myRigidbody.velocity = transform.TransformVector(new Vector3(0, 0, 0)) * speed;
         }
         if (Input.GetKey(KeyCode.DownArrow) == true) // down
         {
-            myRigidbody.velocity = transform.TransformVector(new Vector3(0, -1 * speed, 0)) * Time.deltaTime;
+            myRigidbody.velocity = transform.TransformVector(new Vector3(0, -1, 0)) * speed;
         }
         if (Input.GetKeyUp(KeyCode.DownArrow) == true) // down (disable)
         {
-            myRigidbody.velocity = transform.TransformVector(new Vector3(0, 0, 0));
+            myRigidbody.velocity = transform.TransformVector(new Vector3(0, 0, 0))  * speed;
         }
+
+
         if (Input.GetKey(KeyCode.RightArrow) == true) // right
         {
             myRigidbody.transform.Rotate(0.0f, 0.0f, - rotatespeed * Time.deltaTime);
@@ -44,16 +46,18 @@ public class PlayerScript : MonoBehaviour
         {
             myRigidbody.transform.Rotate(0.0f, 0.0f, rotatespeed * Time.deltaTime);
         }
-        if (Input.GetKeyDown(KeyCode.LeftShift) == true) // sprint
-        {
-            speed = speed * 2;
-            rotatespeed = (float)(rotatespeed * 1.5); // not sure why it has to be done like this, but otherwise Unity complains
-        }
-        if (Input.GetKeyUp(KeyCode.LeftShift) == true) // sprint (disable)
-        {
-            speed = speedoriginal;
-            rotatespeed = rotatespeedoriginal;
-        }
+        // if (Input.GetKeyDown(KeyCode.LeftShift) == true) // sprint
+        // {
+        //     speed = speed * 2;
+        //     rotatespeed = (float)(rotatespeed * 1.5); // not sure why it has to be done like this, but otherwise Unity complains
+        // }
+        // if (Input.GetKeyUp(KeyCode.LeftShift) == true) // sprint (disable)
+        // {
+        //     speed = speedoriginal;
+        //     rotatespeed = rotatespeedoriginal;
+        // }
+
+        
         myRigidbody.angularVelocity = 0; // fix for the circle infinitly rotating after hitting a wall
 
     }
